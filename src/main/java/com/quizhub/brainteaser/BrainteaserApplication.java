7c36deb2063e2067e53874884076ef1eadd242cf
Ankkkkkkit <ankitsingh02003@gmail.com>
2024-08-18 16:34:56 +0530

    Added base spring boot app

---
+++ src/main/java/com/quizhub/brainteaser/BrainteaserApplication.java
@@ -0,0 +1,13 @@
+package com.quizhub.brainteaser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainteaserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainteaserApplication.class, args);
+	}
+
+}
